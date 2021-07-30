7afd9a2173f5a81cc98f58c0f0c12f811d1ade44
KahiBrooks <67168011+Kahi-Brooks@users.noreply.github.com>
2021-07-30 18:47:38 +0000

    noth

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
-// Author:
+//Author: Kahi Brooks
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP;
 
 // Global ML Variables
+let mobilenet;
+let img; 
 
 function setup() {
 
+canvasDiv = createDiv();
+canvas = createCanvas(640, 480);
+canvas.parent(canvasDiv);
+textDiv = createDiv();
+textP = createP("Model loading, please wait...");
+textP.parent(textDiv);
+img = loadImage("images/guinea-pig.jpg");
+const classifier = ml5.imageClassifier('MobileNet', modelLoaded);
 }
 
 function draw() {
 
 }
 
-function gotResults(error, results) {
+function imageLoaded() {
 
 }
+
+function modelReady() {
+mobilenet.classify(canvas, gotResults);
+}
+
+function gotResults(error, results) {
+
+let label = results[0].label;
+let confidence = results[0].confidence;
+const classifier = ml5.imageClassifier('Darknet', modelReady);
+
+textP.html("Label: " + label + " - Confidence " + confidence);
+
+  if(error) {
+
+    console.error(error);
+
+  } else {
+
+    
+
+  }
+
+}
+function modelLoaded() {
+  console.log('Model Loaded!');
+}
+
+
